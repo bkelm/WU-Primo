ed631060a680ceacb489b2572f357ffa92f16cf6
Bill Kelm <bkelm@willamette.edu>
2017-06-06 13:12:41 -0700

    Initial Committ

---
+++ js/1_primo_wu_chat.js
@@ -0,0 +1,8 @@
+/************************************* BEGIN Chat Area ************************************/
+
+applocal.component('prmLogoAfter', {
+    bindings: {},
+    template: '<div class="chat"><a ng-href="http://library.willamette.edu/ref/ask"><img src="http://library.willamette.edu/external/exlibris/primo/images/askLibrarian.png"></img></a></div>'
+});
+
+/************************************* END Chat Area ************************************/
---
+++ js/custom.module.js
@@ -0,0 +1,16 @@
+/************************************* BEGIN Bootstrap Script ************************************/
+
+/* We are a CENTRAL_PACKAGE, so use the below line to bootstrap the module */
+
+ var app = angular.module('centralCustom', ['angularLoad']);
+
+ var applocal = angular.module('viewCustom', ['angularLoad']);
+
+/************************************* END Bootstrap Script ************************************/
+
+
+console.log("from central package");
+
+//enable hide show other institutions
+//enable_hide_show_other_institutions("start_hidden");
+
